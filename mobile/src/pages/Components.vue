cb6246df03c2a30ed1a5c65c53ae82e24302e4e3
Terence Zama <terence.zama@gmail.com>
2023-09-30 09:00:50 +0400

    components standards

--- mobile/src/pages/Components.vue
+++ mobile/src/pages/Components.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { ContentView, CustomLayoutView, StackLayout } from '@nativescript/core';
+
 
 
 
@@ -7,14 +9,36 @@
 
 <template>
     <Page actionBarHidden="false">
-        <ActionBar class="bg-primary">
-            <Label text="Components" class="font-bold text-lg" />
+        <ActionBar class="bg-bzred-500">
+            <Label text="Components" class="font-bold text-lg text-white" />
         </ActionBar>
         <ScrollView orientation="vertical">
             <FlexboxLayout flexDirection="column">
-                <Placeholder />
-                <label text="first" width="70" backgroundColor="#43b883" />
-                <ActivityIndicator busy="true" />
+                <!-- Typography -->
+                <ContentView height="16"></ContentView>
+                <label class="text-3xl text-black" text="Typography" />
+
+                <label class="text-3xl text-shark-500" text="3xl" />
+                <label class="text-2xl text-shark-500" text="2xl" />
+                <label class="text-xl text-shark-500" text="xl" />
+                <label class="text-lg text-shark-500" text="lg" />
+                <label class="text-base text-shark-500" text="base" />
+                <label class="text-sm text-shark-500" text="sm" />
+                <label class="text-xs text-shark-500" text="xs" />
+
+                <!-- Content View  -->
+                <ContentView height="16"></ContentView>
+                <label class="text-3xl text-black " text="Spinner" />
+
+                <ActivityIndicator busy="true" width="24" height="24" class="" />
+
+
+
+
+                <ContentView height="16" class="example"></ContentView>
+                <Button id="myButton" text="{N}" class="animate-bounce" />
+                <label class="text-3xl text-black" text="Loader" />
+
                 <Progress />
                 <button text="Button" />
                 <DatePicker />
@@ -44,3 +68,21 @@
     </Page>
 </template>
 
+
+<style>
+@keyframes example {
+    from {
+        background-color: red;
+    }
+
+    to {
+        background-color: green;
+    }
+}
+
+.view {
+    animation-name: example;
+    animation-duration: 4s;
+    animation-fill-mode: forwards;
+}
+</style>
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{css,xml,html,vue,svelte,ts,tsx}"],
   // use the .ns-dark class to control dark mode (applied by NativeScript) - since 'media' (default) is not supported.
@@ -6,7 +8,34 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#ff0000",
+        primary: "#ff2323",
+        bzred: {
+          50: "#fff0f0",
+          100: "#ffdddd",
+          200: "#ffc0c0",
+          300: "#ff9494",
+          400: "#ff5757",
+          500: "#ff2323",
+          600: "#ff0000",
+          700: "#d70000",
+          800: "#b10303",
+          900: "#920a0a",
+          950: "#500000",
+        },
+        shark: {
+          50: "#f7f7f8",
+          100: "#eeeef0",
+          200: "#dadadd",
+          300: "#babbbf",
+          400: "#94959c",
+          500: "#767681",
+          600: "#606169",
+          700: "#4e4e56",
+          800: "#434349",
+          900: "#3b3c3f",
+          950: "#27272a",
+          DEFAULT: "#27272a",
+        },
       },
     },
   },
