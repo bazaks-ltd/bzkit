d9c77137f268fd5061ab1e6c60d65c0d3733a5d8
Terence Zama <terence.zama@gmail.com>
2023-10-02 07:08:53 +0400

    defined color palettes

--- mobile/src/pages/Components.vue
+++ mobile/src/pages/Components.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
-import { ContentView, CustomLayoutView, FlexboxLayout, StackLayout } from '@nativescript/core';
+import { ContentView, CustomLayoutView, FlexboxLayout, StackLayout, WrapLayout } from '@nativescript/core';
 import FASolidIcons from './demo/FASolidIcons.vue';
 import FABrandsIcons from './demo/FABrandsIcons.vue';
 import FARegularIcons from './demo/FARegularIcons.vue';
-
+import { colors, shark } from '../../colors.cjs'
 
 
 
@@ -29,12 +29,38 @@ import FARegularIcons from './demo/FARegularIcons.vue';
                 <label class="text-base text-shark-500" text="base" />
                 <label class="text-sm text-shark-500" text="sm" />
                 <label class="text-xs text-shark-500" text="xs" />
+                <!-- Shark  -->
+                <ContentView height="16"></ContentView>
+                <label class="text-3xl text-base-content " text="Shades of Shark" />
+                <label class="text-base text-shark-500 leading-none" textWrap="true"
+                    text="Shark is a generic name for grey and will be used for texts" />
+                <WrapLayout>
+                    <FlexboxLayout flexDirection="column" v-for="(k, v) in shark" :key="v" class="w-1/3  p-2 items-center">
+                        <ContentView :backgroundColor="k" class="w-16 h-16"></ContentView>
+                        <label class="text-xs font-bold text-base-content " :text="v" />
+                        <label class="text-xs text-shark-500" :text="k" />
 
+                    </FlexboxLayout>
+                </WrapLayout>
+                <ContentView height="16"></ContentView>
+
+                <!-- Colors  -->
+                <ContentView height="16"></ContentView>
+                <label class="text-3xl text-black " text="Colors" />
+                <WrapLayout>
+                    <FlexboxLayout flexDirection="column" v-for="(k, v) in colors" :key="v" class="w-1/3  p-2 items-center">
+                        <ContentView :backgroundColor="k" class="w-16 h-16"></ContentView>
+                        <label class="text-xs font-bold text-base-content " :text="v" />
+                        <label class="text-xs text-shark-500" :text="k" />
+
+                    </FlexboxLayout>
+                </WrapLayout>
+                <ContentView height="16"></ContentView>
                 <!-- Content View  -->
                 <ContentView height="16"></ContentView>
                 <label class="text-3xl text-black " text="Spinner" />
 
-                <ActivityIndicator busy="true" width="24" height="24" class="" />
+                <ActivityIndicator busy="true" width="24" height="24" :color="colors.primary" />
 
                 <!-- Icons -->
                 <label class="text-3xl text-black " text="Icons" />
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 // const colors = require("tailwindcss/colors");
-const { colors } = require("./colors.cjs");
+const { colors, shark } = require("./colors.cjs");
 
 module.exports = {
   content: ["./src/**/*.{css,xml,html,vue,svelte,ts,tsx}"],
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: colors.primary,
+        ...colors,
         bzred: {
           50: "#fff0f0",
           100: "#ffdddd",
@@ -23,20 +23,7 @@ module.exports = {
           900: "#920a0a",
           950: "#500000",
         },
-        shark: {
-          50: "#f7f7f8",
-          100: "#eeeef0",
-          200: "#dadadd",
-          300: "#babbbf",
-          400: "#94959c",
-          500: "#767681",
-          600: "#606169",
-          700: "#4e4e56",
-          800: "#434349",
-          900: "#3b3c3f",
-          950: "#27272a",
-          DEFAULT: "#27272a",
-        },
+        shark,
       },
     },
   },
