edff315d1908b7ab9a52476ba8a2585ca419480a
Terence Zama <terence.zama@gmail.com>
2023-10-14 15:55:05 +0400

    added automatic keyoard ios

--- mobile/src/app.ts
+++ mobile/src/app.ts
@@ -1,5 +1,30 @@
 import { createApp } from "nativescript-vue";
 import App from "./app.vue";
+import { Application, isIOS } from "@nativescript/core";
 
 const app = createApp(App);
+if (isIOS) {
+  @NativeClass()
+  class MyDelegate extends UIResponder implements UIApplicationDelegate {
+    public static ObjCProtocols = [UIApplicationDelegate];
+
+    applicationDidFinishLaunchingWithOptions(
+      application: UIApplication,
+      launchOptions: NSDictionary<string, any>
+    ): boolean {
+      console.log(
+        "applicationWillFinishLaunchingWithOptions: " + launchOptions
+      );
+
+      IQKeyboardManager.sharedManager().enable = true;
+
+      return true;
+    }
+
+    applicationDidBecomeActive(application: UIApplication): void {
+      console.log("applicationDidBecomeActive: " + application);
+    }
+  }
+  Application.ios.delegate = MyDelegate;
+}
 app.start();
