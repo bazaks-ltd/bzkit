fd9babf4bbc80ba478b9948147893ad43a39f9f3
Terence Zama <terence.zama@gmail.com>
2023-10-18 15:42:41 +0400

    paseto implemetations

--- api/src/paseto/paseto.controller.ts
+++ api/src/paseto/paseto.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { GenerateKeyDto } from './dto/generateKey.dto';
 import { PasetoService } from './paseto.service';
+import { DebugGuard } from 'src/debug/debug.guard';
 
 @Controller('paseto')
+@UseGuards(DebugGuard)
 export class PasetoController {
   constructor(private readonly pasetoService: PasetoService) {}
   @Post('generateKey')
@@ -11,5 +13,25 @@ export class PasetoController {
     data: GenerateKeyDto,
   ): Promise<string | { secretKey: string; publicKey: string }> {
     return this.pasetoService.generateKey(data.purpose);
+  }
+
+  @Post('sign')
+  async sign(@Body() data: any): Promise<any> {
+    return this.pasetoService.sign(data);
+  }
+
+  @Post('verify')
+  async verify(@Body() { token }): Promise<any> {
+    return this.pasetoService.verify(token);
+  }
+
+  @Post('encrypt')
+  async encrypt(@Body() data: any): Promise<any> {
+    return this.pasetoService.encrypt(data);
+  }
+
+  @Post('decrypt')
+  async decrypt(@Body() { token }): Promise<any> {
+    return this.pasetoService.decrypt(token);
   }
 }
--- api/src/paseto/paseto.service.ts
+++ api/src/paseto/paseto.service.ts
@@ -10,4 +10,24 @@ export class PasetoService {
       return generateKey('public', { format: 'paserk' });
     }
   }
+
+  public async sign(payload: Buffer | Record<PropertyKey, unknown>) {
+    const { sign } = paseto.V3;
+    return sign(payload, process.env.PASETO_SECRET);
+  }
+
+  public async verify(token: string) {
+    const { verify } = paseto.V3;
+    return verify(token, process.env.PASETO_PKEY);
+  }
+
+  public async encrypt(payload: Buffer | Record<PropertyKey, unknown>) {
+    const { encrypt } = paseto.V3;
+    return encrypt(payload, process.env.PASETO_LKEY);
+  }
+
+  public async decrypt(token: string) {
+    const { decrypt } = paseto.V3;
+    return decrypt(token, process.env.PASETO_LKEY);
+  }
 }
